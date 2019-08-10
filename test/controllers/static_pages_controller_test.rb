f7f354b0c9bc0a6f2b2d31c4b09ee7f8d7a90b50
sackey24 <sackeyad24@gmail.com>
2019-08-10 13:25:09 +0900

    Finish layout and routes

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,29 +1,24 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
-  test "should get root" do
-    get root_url
-    assert_response :success
-  end
-  
+
   test "should get home" do
-    get static_pages_home_url
+    get root_path
     assert_response :success
   end
 
   test "should get help" do
-    get static_pages_help_url
+    get help_path
     assert_response :success
   end
 
   test "should get about" do
-    get static_pages_about_url
+    get about_path
     assert_response :success
   end
 
   test "should get contact" do
-    get static_pages_contact_url
+    get contact_path
     assert_response :success
   end
-
 end
---
+++ test/helpers/application_helper_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class ApplicationHelperTest < ActionView::TestCase
+  test "full title helper" do
+    assert_equal full_title,         "AtCoder Rivals"
+  end
+end
