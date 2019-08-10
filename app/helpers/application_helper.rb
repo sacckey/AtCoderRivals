476e7b7c78bed4375cf9e9e2ab503510db41e27e
sackey24 <sackeyad24@gmail.com>
2019-08-10 10:50:17 +0900

    Update page title

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  # ページごとの完全なタイトルを返します。
+  def full_title(page_title = '')
+    base_title = "AtCoder Rivals"
+    if page_title.empty?
+      base_title
+    else
+      page_title + " | " + base_title
+    end
+  end
 end
