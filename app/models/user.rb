3eb12da74f4ab0794798f6ae636da53de481504c
sackey24 <sackeyad24@gmail.com>
2019-10-10 22:14:15 +0900

    Add validate :atcoder_id_exist

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,13 +4,14 @@ class User < ApplicationRecord
   validates :uid, presence: true, uniqueness: { case_sensitive: false }
   validates :user_name, presence: true
   validates :atcoder_id, presence: { message: "ID can't be blank" }
+  validate :atcoder_id_exist
 
   def self.find_or_create_from_auth(auth)
     provider = auth[:provider]
     uid = auth[:uid]
     user_name = auth[:info][:name]
     image_url = auth[:info][:image]
-    atcoder_id = user_name
+    atcoder_id = "chokudai"
 
     self.find_or_create_by(provider: provider, uid: uid) do |user|
       user.user_name = user_name
@@ -18,4 +19,36 @@ class User < ApplicationRecord
       user.atcoder_id ||= atcoder_id
     end
   end
+
+
+  private
+
+    def atcoder_id_exist
+      result = get_accepted_count(self)
+      errors.add(:atcoder_id, "IDが存在しません。") if result.nil?
+    end
+
+    def get_accepted_count(user)
+      uri = URI.parse("https://kenkoooo.com/atcoder/atcoder-api/v2/user_info?user=#{user.atcoder_id}")
+      result = call_api(uri)
+      if !result.nil?
+        ac_count=result["accepted_count"]
+      end
+    end
+
+    def call_api(uri)
+      https = Net::HTTP.new(uri.host, uri.port)
+      https.use_ssl = true
+      res = https.start do
+        https.get(uri.request_uri)
+      end
+      if res.code == '200'
+        result = JSON.parse(res.body)
+      else
+        puts "#{res.code} #{res.message}"
+        puts "No such user_id"
+        puts "test"
+        # exit 1
+      end
+    end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -2,7 +2,7 @@ User.create!(user_name:  "Example User",
   uid: "12345",
   provider:  "twitter",
   image_url: "https://pbs.twimg.com/profile_images/1066244463725445120/m-owVBJX_normal.jpg",
-  atcoder_id: "Yama24")
+  atcoder_id: "chokudai")
 
 99.times do |n|
 user_name  = Faker::Name.name
