3c21511871218aa63f82aace6cbf8ec42e0974d7
sacckey <sackeyad24@gmail.com>
2020-09-25 08:29:16 +0900

    Refactoring initialize atcoder user

--- app/controllers/atcoder_users_controller.rb
+++ app/controllers/atcoder_users_controller.rb
@@ -10,14 +10,6 @@ class AtcoderUsersController < ApplicationController
       atcoder_id = m[1]
     end
 
-    @atcoder_user = AtcoderUser.find_or_initialize_by(atcoder_id: atcoder_id)
-    @atcoder_user.set_info if is_new_user = @atcoder_user.new_record?
-
-    if @atcoder_user.valid? && is_new_user
-      # TODO: sidekiqに積む
-      api_client = APIClient.new
-      api_client.get_user_history(@atcoder_user)
-      api_client.get_user_submissions(@atcoder_user)
-    end
+    @atcoder_user = AtcoderUser.find_or_create_by(atcoder_id: atcoder_id)
   end
 end
--- test/models/atcoder_user_test.rb
+++ test/models/atcoder_user_test.rb
@@ -2,16 +2,12 @@ require 'test_helper'
 
 class AtcoderUserTest < ActiveSupport::TestCase
   def setup
-    @atcoder_user = AtcoderUser.new(atcoder_id: 'Petr', accepted_count: 10,
-      accepted_count_rank: 3, rated_point_sum: 1000.0, rated_point_sum_rank: 3,
-      image_url: 'https://img.atcoder.jp/icons/6a4376b4c8a89762d6153dc31b711a36.jpg',
-      rating: 1000, etag: '')
+    @atcoder_user = AtcoderUser.create(atcoder_id: 'Petr')
   end
 
   test "associated microposts should be destroyed" do
-    @atcoder_user.save
-    @atcoder_user.submissions.create!(epoch_second: 1575190800, language: "Java", point: 1000.0, result: "AC", contest_name: "abc100", problem_name: "abc100_a", number: 3)
-    assert_difference 'Submission.count', -1 do
+    submission_count = @atcoder_user.submissions.count
+    assert_difference 'Submission.count', -submission_count do
       @atcoder_user.destroy
     end
   end
