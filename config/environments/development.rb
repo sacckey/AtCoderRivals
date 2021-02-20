5f4783daefbe9648d66efdd5c4702e212489e491
sacckey <sackeyad24@gmail.com>
2021-02-20 10:36:45 +0900

    Add twitter-uid column to users

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -48,7 +48,9 @@ Rails.application.configure do
   # Debug mode disables concatenation and preprocessing of assets.
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
-  config.assets.debug = true
+  config.assets.debug = false
+
+  config.web_console.whitelisted_ips = '0.0.0.0/0'
 
   # Suppress logger output for asset requests.
   config.assets.quiet = true
